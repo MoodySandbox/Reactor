b1410c7fad907ceeba9949d35462b23d627b32b5
Moody <hi@moodyboles.com>
2024-07-03 22:53:38 +0100

    add proposed ideas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,18 @@ const Home: React.FC = () => {
 				<h1 className="text-2xl font-bold text-white my-2">Reactor</h1>
 				<p className="mb-5 text-gray-400">Click anywhere on the image to leave a comment</p>
 				<Canvas />
-				<p className="mt-5 text-gray-400 hover:text-white text-xs">
+				<hr className="my-5 border-gray-600" />
+				<div className="text-gray-200">
+					<h6>Potential Future Implementations:</h6>
+					<ul className="list-disc list-inside mt-2">
+						<li>Extend canvas media to accept videos and reacting to specific frames.</li>
+						<li>Implement a backend to store comments and reactions.</li>
+						<li>Allow replying to comments/threads.</li>
+						<li>Use emojis library instead of the limited emojis options.</li>
+					</ul>
+				</div>
+				<hr className="my-5 border-gray-600" />
+				<p className="text-gray-400 hover:text-white text-xs">
 					<a href="https://moodyboles.com/?ref=reactor" target="_blank">@moodyboles</a>
 				</p>
 			</div>
